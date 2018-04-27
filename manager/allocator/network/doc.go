dbb2b9dda2748ce88f23d4b430f4e49bc722e468
Drew Erny <drew.erny@docker.com>
2018-04-27 14:24:51 -0700

    Add network allocator
    Adds the highest-level component charged with allocating network
    resources from from the underlying libraries. The network allocator
    provides an interface between the higher level swarmkit objects
    (services, tasks and nodes), and the lower level swarmkit objects
    (endpoints, attachments) that the lower level-allocator components
    operate on.
    
    Signed-off-by: Drew Erny <drew.erny@docker.com>

---
+++ manager/allocator/network/doc.go
@@ -0,0 +1,96 @@
+package network
+
+// The allocator/network package is the subcomponent in charge of allocating
+// network resources for swarmkit resources. Give it objects, and it gives you
+// resources.
+//
+// In swarmkit you'll see two kinds of objects: components with an event loop,
+// and components which are strictly accessed through method calls. This is the
+// latter.
+//
+// The network allocator doesn't have direct access to the raft store. The
+// caller will provide all of the objects, and be in charge of committing those
+// objects to the raft store. Instead, it works directly on the api objects,
+// and gives them back to the caller to figure out how to store. This frees us
+// from having to worry about the overlying storage method, and reduces the
+// already rather bloated surface are of this component
+//
+// The network allocator provideds the interface for the top-level API objets,
+// and lower level component allocators operate on specific fields.
+//
+// The network package fully owns a few fields, and should be the _only_
+// component that ever writes to them. Conversely, these are the _only_ fields
+// that the Allocator ever writes to, meaning it can safely work on an object
+// concurrently with other routines, (as long as they don't try to read these
+// fields) and the fields it owns can be merged into objects safely. These
+// fields are:
+//
+// - api.Network:
+//   - DriverState
+//   - IPAM
+// - api.Node:
+//   - Attachment
+//   - Attachments
+// - api.Service
+//   - Endpoint
+// - api.Task
+//   - Networks
+//   - Endpoint
+//
+// These are essentially all of the objects of types:
+// - api.Driver
+// - api.IPAMOptions
+// - api.NetworkAttachment
+// - api.Endpoint
+//
+// The allocator's methods always either fully allocate the object and return
+// nil, or return an error and leave the object unaltered. The allocator will
+// never leave objects in a partially-allocated state.
+//
+// # Local state
+//
+// The network allocator, and its subcomponents, rely heavily on internal,
+// local state for many reasons. All of the libnetwork libraries that the
+// network allocator depends on also have their own local state. If the local
+// state of the allocator is not completely consistent with the distributed
+// state, then errors such as double allocations or use-after-frees may result.
+//
+// In order to correctly allocate, the allocator provides a Restore method,
+// to which all allocated and unallocated objects can be passed. The Restore
+// method runs through the state of all objects (and calls Restore methods on
+// the subcomponents) to fully initialize the local state of the allocator. If
+// the Restore method returns no errors, then the local state has been fully,
+// correctly and consistently restored, and new allocation can proceed
+// correctly. The Restore method should only be called once; subsequent calls
+// have undefined behavior.
+//
+// # Subcomponents
+//
+// The real work of the allocator is done in the subcomponents, the driver,
+// ipam, and port packages. These components ere each concerned with specific
+// parts of the network objects, and own the corresponding fields. The specific
+// documentation for those components is located within their packages, but at
+// a high level, they are described below
+//
+// ## `driver`
+//
+// The `driver` package is concerned with allocating network driver state for
+// networks.  It only operates on Network objects, and totally owns their
+// Driver field.
+//
+// ## `ipam`
+//
+// The `ipam` package is concerned with allocating IP addresses and IPAM driver
+// state. It is the most complex package, and contains the most code. It
+// provides the translation between the underlying libnetwork ipam libraries,
+// which operates in terms of addresses and pools, and the swarmkit objects
+// that require those things. The ipam package fully owns any NetworkAttachment
+// fields, and fully owns the Endpoint's Virtual IPs. It also fully owns the
+// IPAMConfig object for networks.
+//
+// ## `port`
+//
+// The port allocator, uniquely, does not rely on libnetwork. Its
+// responsibility is to choose port numbers from the available port range, keep
+// track of which ports are in use, and return errors to prevent overlapping
+// port allocations.
